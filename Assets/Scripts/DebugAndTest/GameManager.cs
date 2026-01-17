a8c61a4f7897634e4b87af7c0ac2a1f084007957
Dmitry <d.chernenko@trace-x.ru>
2026-01-17 21:51:04 +0300

    +

--- Assets/Scripts/DebugAndTest/GameManager.cs
+++ Assets/Scripts/DebugAndTest/GameManager.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts.DebugAndTest
         {
             var data = _worldDataHolder.Data;
 
-            //Subscribe();
+            Subscribe();
 
             data.SnapshotApplied += Subscribe;
         }
